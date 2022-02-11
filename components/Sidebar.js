58d2773a388380e1b3028e8139df76ae288bb38b
levaldiv <luis.edvaldivia@gmail.com>
2022-02-11 12:02:58 -0800

    repositioned

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -10,7 +10,7 @@ import Image from "next/image";
 
 function Sidebar() {
   return (
-    <section className="fixed top-0 z-40 flex h-screen w-[90px] flex-col items-center space-y-8 bg-black p-4">
+    <section className="fixed top-0 z-40 flex h-screen w-[80px] flex-col items-center space-y-8 bg-black p-4">
       <Image
         src="https://rb.gy/nw0evo"
         width={45}
