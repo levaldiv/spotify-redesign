234967ed4e7f70fc2fc6925ea3ab8d4a7eed7996
levaldiv <luis.edvaldivia@gmail.com>
2022-02-11 12:27:55 -0800

    styled search bar

--- components/Body.js
+++ components/Body.js
@@ -2,7 +2,7 @@ import Search from "./Search";
 
 function Body() {
   return (
-    <section>
+    <section className="ml-24 flex-grow space-y-8 bg-black py-4 md:mr-2.5 md:max-w-6xl">
       {/* This is the search bar */}
       <Search />
     </section>
--- components/Search.js
+++ components/Search.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
 
 function Search() {
   return (
-    <div>Search</div>
-  )
+    <div className="flex max-w-[1150px] items-center overflow-hidden rounded-full border-2 border-[#333333] bg-[#1A1A1A] p-1.5 px-5 pr-8">
+      <div className="h-4 w-4 flex-shrink-0 animate-pulse rounded-full border-2">
+        <input
+          type="text"
+          className="border-none bg-[#1A1A1A] text-xs text-white placeholder-[#FAFAFA] outline-none focus:ring-0 lg:w-full"
+          placeholder="Search for a song..."
+        />
+      </div>
+    </div>
+  );
 }
 
-export default Search
+export default Search;
