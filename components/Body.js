92dfc6d551dabcb2465ea3c7550f989152107fd5
levaldiv <luis.edvaldivia@gmail.com>
2022-02-11 13:12:30 -0800

    sending search setsearch to search comp

--- components/Body.js
+++ components/Body.js
@@ -1,10 +1,13 @@
+import { useState } from "react";
 import Search from "./Search";
 
 function Body() {
+  const [search, setSearch] = useState("");
+  
   return (
     <section className="ml-24 flex-grow space-y-8 bg-black py-4 md:mr-2.5 md:max-w-6xl">
       {/* This is the search bar */}
-      <Search />
+      <Search search={search} setSearch={setSearch} />
     </section>
   );
 }
