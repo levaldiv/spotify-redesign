3804cd813f316f8baa7d53ef0f16363260d897ad
levaldiv <luis.edvaldivia@gmail.com>
2022-02-11 15:30:17 -0800

    added styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,28 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        "2xl": "1500px",
+      },
+      keyframes: {
+        pulse: {
+          "0%, 100%": {
+            opacity: 1,
+          },
+          "50%": {
+            opacity: 0.5,
+          },
+        },
+      },
+      animation: {
+        pulse: "pulse 4s cubic-bezier(0.4, 0, 0.6, 1) infinite",
+      },
+    },
+    fontFamily: {
+      body: ["Andada Pro", "serif"],
+      sans: ["ui-sans-serif", "system-ui"],
+    },
   },
   plugins: [
     require("tailwind-scrollbar-hide"),
