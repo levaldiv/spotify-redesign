5be42a49a7a548c0a5ce75ebcaf6cde1d5f6c449
levaldiv <luis.edvaldivia@gmail.com>
2022-02-11 14:38:55 -0800

    added btns,styled

--- components/Search.js
+++ components/Search.js
@@ -1,16 +1,30 @@
-import React from "react";
+import { MdOutlineShortText } from "react-icons/md";
 
-function Search() {
+function Search({ search, setSearch }) {
   return (
     <div className="flex max-w-[1150px] items-center overflow-hidden rounded-full border-2 border-[#333333] bg-[#1A1A1A] p-1.5 px-5 pr-8">
-      <div className="h-4 w-4 flex-shrink-0 animate-pulse rounded-full border-2">
-        <input
-          type="text"
-          value={search}
-          onChange={(e) => setSearch(e.target.value)}
-          className="border-none bg-[#1A1A1A] text-xs text-white placeholder-[#FAFAFA] outline-none focus:ring-0 lg:w-full"
-          placeholder="Search for a song..."
-        />
+      <div className="h-4 w-4 flex-shrink-0 animate-pulse rounded-full border-2" />
+      
+      <input
+        type="text"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+        // focus:ring-0 disables the highlighting of the text box when clicked
+        className="border-none bg-[#1A1A1A] text-xs text-white placeholder-[#FAFAFA] outline-none focus:ring-0 lg:w-full"
+        placeholder="Search for a song..."
+      />
+
+      <div className="ml-auto flex items-center divide-x-2 divide-dotted divide-[#333333]">
+        <div className="flex space-x-2 pr-5">
+          <button className="tag">Minimal</button>
+          <button className="tag">House</button>
+          <button className="tag">Minimal</button>
+        </div>
+
+        <div className="flex items-center space-x-1.5 pl-4 text-[#CECECE]">
+          <MdOutlineShortText className="animate-pulse text-2xl" />
+          <span className="text-sm font-medium">Filters</span>
+        </div>
       </div>
     </div>
   );
