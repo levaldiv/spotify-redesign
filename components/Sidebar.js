338d6daa3504f6134e7a892dafefc62dc7e8d911
levaldiv <luis.edvaldivia@gmail.com>
2022-02-11 11:02:16 -0800

    added image

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import Image from "next/image";
+import React from "react";
 
 function Sidebar() {
   return (
-    <div>Sidebar</div>
-  )
+    <section>
+      <Image
+        src="https://rb.gy/xkacau"
+        width={56}
+        height={56}
+        objectFit="contain"
+      />
+    </section>
+  );
 }
 
-export default Sidebar
+export default Sidebar;
