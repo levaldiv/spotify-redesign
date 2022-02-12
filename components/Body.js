9fcee12932ffd2500c3a0ab5c75b258b16567ce6
levaldiv <luis.edvaldivia@gmail.com>
2022-02-11 16:13:35 -0800

    styled

--- components/Body.js
+++ components/Body.js
@@ -3,11 +3,17 @@ import Search from "./Search";
 
 function Body() {
   const [search, setSearch] = useState("");
-  
+  const [searchResults, setSearchResults] = useState([]);
+
   return (
     <section className="ml-24 flex-grow space-y-8 bg-black py-4 md:mr-2.5 md:max-w-6xl">
       {/* This is the search bar */}
       <Search search={search} setSearch={setSearch} />
+
+      {/* Canvas of newest music */}
+      <div className="scrollbar-hide grid h-96 grid-cols-2 gap-x-4 gap-y-8 overflow-y-scroll p-4 py-4 lg:grid-cols-3 xl:grid-cols-4">
+        
+      </div>
     </section>
   );
 }
