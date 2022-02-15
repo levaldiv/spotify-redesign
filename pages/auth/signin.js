697a307fcb105c5408f0f6251b88759ee3aac2ed
levaldiv <luis.edvaldivia@gmail.com>
2022-02-15 15:07:51 -0800

    fetching providers

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import Head from "next/head";
+import Image from "next/image";
 
 function Signin() {
   return (
-    <div>Signin</div>
-  )
+    <div>
+      <Head>
+        <title>Spotify - Login</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Image
+        src="https://rb.gy/y9mwtb"
+        height={250}
+        width={600}
+        objectFit="contain"
+        className="animate-pulse"
+      />
+    </div>
+  );
 }
 
-export default Signin
+export default Signin;
+
+export async function getServerSideProps() {
+  const providers = await getProviders();
+  return {
+    props: { providers },
+  };
+}
