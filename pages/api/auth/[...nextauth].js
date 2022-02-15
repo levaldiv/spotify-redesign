600afbd3df484e5174a997920ef584bb69c0a984
levaldiv <luis.edvaldivia@gmail.com>
2022-02-15 12:33:55 -0800

    added callback

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -7,6 +7,9 @@ export default NextAuth({
     SpotifyProvider({
       clientId: process.env.SPOTIFY_CLIENT_ID,
       clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
+      //   adding scopes
+      authorization:
+        "https://accounts.spotify.com/authorize?scope=user-read-email,playlist-read-private,user-read-email,streaming,user-read-private,user-library-read,user-library-modify,user-read-playback-state,user-modify-playback-state,user-read-recently-played,user-follow-read",
     }),
     // ...add more providers here
   ],
@@ -14,5 +17,35 @@ export default NextAuth({
   //   creating custom login page
   pages: {
     signIn: "/auth/signin",
+  },
+
+  //   Adding callbacks
+  callbacks: {
+    async jwt({ token, user, account }) {
+      // Initial sign in
+      if (account && user) {
+        return {
+          accessToken: account.access_token,
+          accessTokenExpires: Date.now() + account.expires_in * 1000,
+          refreshToken: account.refresh_token,
+          user,
+        };
+      }
+
+      //   Return previous token if the current access token hasnt expired yet
+      if (Date.now() < token.accessTokenExpires) {
+        return token;
+      }
+
+      // if access token expired, try to update it
+      return refreshAccessToken(token);
+    },
+    async session({ session, token }) {
+      session.user = token.user;
+      session.accessToken = token.accessToken;
+      session.error = token.error;
+
+      return session;
+    },
   },
 });
