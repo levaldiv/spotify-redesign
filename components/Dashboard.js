7bfc682b7d61a103b9ad94fc820935f46532f411
levaldiv <luis.edvaldivia@gmail.com>
2022-02-16 12:14:17 -0800

    fetching client id

--- components/Dashboard.js
+++ components/Dashboard.js
@@ -1,13 +1,20 @@
 import React from "react";
+import SpotifyWebApi from "spotify-web-api-node";
 import Body from "./Body";
 import Right from "./Right";
 import Sidebar from "./Sidebar";
+
+const spotifyApi = new SpotifyWebApi({
+  // fetching users client id when they are signed in
+  clientId: process.env.SPOTIFY_CLIENT_ID,
+});
 
 function Dashboard() {
   return (
     <main>
       <Sidebar />
-      <Body />
+      {/* sending that id to the body */}
+      <Body spotifyApi={spotifyApi} />
       <Right />
     </main>
   );
