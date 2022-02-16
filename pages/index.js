3b2b9460b5c82afcb9003497079c7d9650379ce6
levaldiv <luis.edvaldivia@gmail.com>
2022-02-15 18:03:58 -0800

    created loader file, added onUnauth fcn

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,22 @@
+import { useSession } from "next-auth/react";
 import Head from "next/head";
+import { useRouter } from "next/router";
 import Dashboard from "../components/Dashboard";
+import Loader from "../components/Loader";
 
 export default function Home() {
+  const router = useRouter();
+  const { status, data: session } = useSession({
+    // This ensures that no one can login without sigining in first
+    onUnauthenticated() {
+      router.push("/auth/signin");
+    },
+  });
+
+  if (status === "loading") {
+    return <Loader />;
+  }
+
   return (
     <div>
       <Head>
