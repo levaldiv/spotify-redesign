b71b3aec513efc1b91b7e0d0766d21b6e9f77614
levaldiv <luis.edvaldivia@gmail.com>
2022-02-16 16:22:36 -0800

    comments

--- atoms/playerAtom.js
+++ atoms/playerAtom.js
@@ -1,11 +1,13 @@
 import { atom } from "recoil";
 
 // this provides access to the whole app
+// if the song is playing or not
 export const playState = atom({
   key: "playState",
   default: false,
 });
 
+// which track is currently playing
 export const playingTrackState = atom({
   key: "playingTrackState",
   default: "",
