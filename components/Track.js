69e7ee061b513112c18d1a4414e2a0e262ac8f04
levaldiv <luis.edvaldivia@gmail.com>
2022-02-17 14:23:25 -0800

    styled tracks

--- components/Track.js
+++ components/Track.js
@@ -1,5 +1,15 @@
-function Track() {
-  return <div></div>;
+function Track({ track }) {
+  return (
+    <div className="group flex cursor-default items-center justify-between space-x-20 rounded-lg py-2 px-4 transition ease-out hover:bg-white/10">
+      <div className="flex items-center">
+        <img
+          src={track.albumUrl}
+          alt=""
+          className="mr-3 h-12 rounded-xl object-cover"
+        />
+      </div>
+    </div>
+  );
 }
 
 export default Track;
