3ea1d2321122b1aebfdf9ddcaff4d8ff6aa48bf2
levaldiv <luis.edvaldivia@gmail.com>
2022-02-16 17:06:41 -0800

    created global css class

--- components/Body.js
+++ components/Body.js
@@ -92,6 +92,24 @@ function Body({ spotifyApi, chooseTrack }) {
                 />
               ))}
       </div>
+
+      <div className="absolute ml-6 flex min-w-full gap-x-8 md:relative">
+        {/* Creating genres */}
+        <div className="hidden max-w-[270px] xl:inline">
+          <h2 className="mb-3 font-bold text-white">Genres</h2>
+          <div className="mb-3 flex flex-wrap gap-x-2 gap-y-2.5">
+            <div className="genre">Classic</div>
+            <div className="genre">House</div>
+            <div className="genre">Minimal</div>
+            <div className="genre">Hip-hop</div>
+            <div className="genre">Electronic</div>
+            <div className="genre">Chillout</div>
+            <div className="genre">Blues</div>
+            <div className="genre">Country</div>
+            <div className="genre">Techno</div>
+          </div>
+        </div>
+      </div>
     </section>
   );
 }
