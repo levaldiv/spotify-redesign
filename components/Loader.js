2c95677f761a74ac9031b27a9f17bfd6f8c6149b
levaldiv <luis.edvaldivia@gmail.com>
2022-02-18 13:03:05 -0800

    created loader

--- components/Loader.js
+++ components/Loader.js
@@ -1,7 +1,22 @@
-import React from "react";
+import { ThreeBounce } from "better-react-spinkit";
+import Image from "next/image";
 
 function Loader() {
-  return <div>Loading...</div>;
+  return (
+    <div className="h-screen bg-black">
+      <div className="flex flex-col items-center space-y-4 pt-40">
+        <span className="relative h-[250px] w-[400px] lg:h-[240px] lg:w-[550px]">
+          <Image
+            src="https://rb.gy/y9mwtb"
+            layout="fill"
+            objectFit="contain"
+            className="animate-pulse"
+          />
+        </span>
+        <ThreeBounce size={23} color="#15883e" />
+      </div>
+    </div>
+  );
 }
 
 export default Loader;
