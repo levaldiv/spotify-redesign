7f3478c5aa9887436424d4df9d54f0a07e466fee
levaldiv <luis.edvaldivia@gmail.com>
2022-02-17 16:22:32 -0800

    styled recently platyed

--- components/RecentlyPlayed.js
+++ components/RecentlyPlayed.js
@@ -1,7 +1,20 @@
 import React from "react";
+import { useRecoilState } from "recoil";
+import { playingTrackState, playState } from "../atoms/playerAtom";
 
-function RecentlyPlayed() {
-  return <div>RecentlyPlayed</div>;
+function RecentlyPlayed({ track, chooseTrack }) {
+  const [play, setPlay] = useRecoilState(playState);
+  const [playingTrack, setPlayingTrack] = useRecoilState(playingTrackState);
+
+  return (
+    <div>
+      <img
+        src={track.albumUrl}
+        alt=""
+        className="h-[52px] w-[52px] rounded-full "
+      />
+    </div>
+  );
 }
 
 export default RecentlyPlayed;
